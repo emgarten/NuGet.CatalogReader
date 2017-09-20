37c18c26a077bff8465951acf1744493cd30db4a
Ashish Jain (NUGET) <asja@microsoft.com>
2017-09-20 11:50:14 -0700

    Added semver2 registration url

--- src/NuGet.CatalogReader/NuGetExtensions.cs
+++ src/NuGet.CatalogReader/NuGetExtensions.cs
@@ -87,7 +87,7 @@ namespace NuGet.CatalogReader
             return uri.AbsolutePath.Replace("/", "_").Replace("\\", "_").Replace(":", "_");
         }
 
-        private static readonly string[] RegistrationsBaseUrl = { "RegistrationsBaseUrl/3.4.0", "RegistrationsBaseUrl/3.0.0-beta" };
+        private static readonly string[] RegistrationsBaseUrl = { "RegistrationsBaseUrl/3.6.0", "RegistrationsBaseUrl/3.4.0", "RegistrationsBaseUrl/3.0.0-beta" };
         private static readonly string[] PackageBaseAddressUrl = { "PackageBaseAddress/3.0.0" };
         private static readonly string[] CatalogServiceUrl = { "Catalog/3.0.0", "http://schema.emgarten.com/sleet#Catalog/1.0.0" };
 
