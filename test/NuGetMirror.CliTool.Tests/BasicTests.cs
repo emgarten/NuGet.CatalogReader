50cd31306c5ad3cbea1b7ad8f856fa337da15c39
Justin Emgarten <justin@emgarten.com>
2021-11-19 22:21:45 -0800

    Update to dotnet 6.0

--- test/NuGetMirror.CliTool.Tests/BasicTests.cs
+++ test/NuGetMirror.CliTool.Tests/BasicTests.cs
@@ -50,7 +50,7 @@ namespace NuGetMirror.CliTool.Tests
                 var result = await CmdRunner.RunAsync(dotnetExe, testContext.Root, $"tool install nugetmirror --version {version} --add-source {nupkgsFolder} --tool-path {dir}");
                 result.Success.Should().BeTrue(result.AllOutput);
 
-                var dllPath = Path.Combine(dir, ".store", "nugetmirror", version, "nugetmirror", version, "tools", "net5.0", "any", "NuGetMirror.dll");
+                var dllPath = Path.Combine(dir, ".store", "nugetmirror", version, "nugetmirror", version, "tools", "net6.0", "any", "NuGetMirror.dll");
 
                 if (!File.Exists(dllPath))
                 {
